ed535e2604641948bf8eff08bbe2f717aef687ae
Brodecks <raphae.donikian@oulootk.fr>
2025-11-05 01:20:28 +0100

    Phase 12 : ORM (formulaire pour modifier une visite)

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -60,4 +60,14 @@ class VisiteRepository extends ServiceEntityRepository
         $this->getEntityManager()->remove($visite);
         $this->getEntityManager()->flush();
     }
+    
+    /**
+     * ajout ou modif d'une visite
+     * @param Visite $visite
+     * @return void
+     */
+    public function add(Visite $visite):void{
+        $this->getEntityManager()->persist($visite);
+        $this->getEntityManager()->flush();
+    }
 }

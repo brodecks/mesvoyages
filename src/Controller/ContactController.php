0722edf0e10a0382b778982a951a0cef7b0b8d3a
Brodecks <raphae.donikian@oulootk.fr>
2025-11-11 22:46:34 +0100

    Phase 24 : déploiement

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -39,7 +39,7 @@ class ContactController extends AbstractController{
     public function sendEmail(MailerInterface $mailer, Contact $contact){
         $email = (new Email())
             ->from($contact->getEmail())
-            ->to('contact@mesvoyages.com')
+            ->to('raphael.donikian@outlook.fr')
             ->subject('Message du site de voyages')
             ->html($this->renderView(
                     'pages/_email.html.twig', [
